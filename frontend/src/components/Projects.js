396e3880a4e6a5ac1fdfde7dcd5c47dda6572523
reemi7 <raheemchaudhry3@gmail.com>
2024-08-29 03:44:19 +0500

    add first wesite  github link in  project

--- frontend/src/components/Projects.js
+++ frontend/src/components/Projects.js
@@ -1,6 +1,6 @@
 import { Container, Row, Col, Tab, Nav } from "react-bootstrap";
 import { ProjectCard } from "./ProjectCard";
-import projImg1 from "../assets/img/project-img1.png";
+import projImg1 from "../assets/img/social-media-web.png";
 import projImg2 from "../assets/img/project-img2.png";
 import projImg3 from "../assets/img/project-img3.png";
 import colorSharp2 from "../assets/img/color-sharp2.png";
@@ -14,7 +14,7 @@ export const Projects = () => {
       title: "Business Startup",
       description: "Design & Development",
       imgUrl: projImg1,
-      githubUrl: "https://github.com/your-github-url/business-startup" // Update with actual URL
+      githubUrl: "https://reemi7.github.io/Social-Media-website/" // Update with actual URL
     },
     {
       title: "Business Startup",
