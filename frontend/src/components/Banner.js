ea49a3648c99b23c8b6d0ddd5b1e412487e0e364
reemi7 <raheemchaudhry3@gmail.com>
2024-10-24 01:21:41 +0500

    add resume link

--- frontend/src/components/Banner.js
+++ frontend/src/components/Banner.js
@@ -46,6 +46,11 @@ export const Banner = () => {
       setIndex(prevIndex => prevIndex + 1);
     }
   }
+  let link = "https://drive.google.com/drive/my-drive"
+
+  const open_resmue = () => {
+    window.open(link, '_blank');
+  }
 
   return (
     <section className="banner" id="home">
@@ -58,7 +63,7 @@ export const Banner = () => {
                 <span className="tagline">Welcome to my Portfolio</span>
                 <h1>{`Hi! I'm Raheem`} <span className="txt-rotate" dataPeriod="1000" data-rotate='[ "Web Developer", "Web Designer", "UI/UX Designer" ]'><span className="wrap">{text}</span></span></h1>
                   <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
-                  <button onClick={() => console.log('connect')}>My resume <ArrowRightCircle size={25} /></button>
+                  <button onClick={() => open_resmue()}>My resume <ArrowRightCircle size={25} /></button>
               </div>}
             </TrackVisibility>
           </Col>
